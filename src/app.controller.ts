e1b9e2cdfd291508c7fd07fe44c79d0d5e81d2d4
gigihsigap <gigihsigap@gmail.com>
2023-10-24 22:08:12 +0700

    Test push to Dockerhub

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/anjay')
+  getAnjay(): string {
+    return this.appService.getAnjay();
+  }
 }
